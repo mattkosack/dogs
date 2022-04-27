f6829d699a5e2aa639893831dd7e72a3fcb7c4ff
Matthew Kosack <kosackm@moravian.edu>
2022-04-27 16:36:21 -0400

    started using fetch

--- dogs.js
+++ dogs.js
@@ -1,8 +1,22 @@
+function get_image_folders(link) {
+    fetch(link)
+    .then(response => response.json())
+    .then(data => {
+        return data
+    })
+    .catch(error => console.error(error))
+}
+
+
 function get_random_image() {
-    let link = "https://api.github.com/repos/mattkosack/dogs";
-    var images = []
-    var random_index = Math.floor(Math.random() * images.length);
-    return images[random_index];
+    let link = "https://api.github.com/repos/mattkosack/dogs/contents";
+    let folders = get_image_folders(link);
+    console.log(folders);
+
+    // var images = []
+    // var random_index = Math.floor(Math.random() * images.length);
+    // return images[random_index];
+    return "images/n02091467-Norwegian_elkhound/n02091467_92.jpg"
 }
 
 
@@ -16,7 +30,3 @@ function show_image() {
     // img.alt = alt;
     document.body.appendChild(img);
 }
-
-
-
-// Compare: https://docs.github.com/en/rest/reference/repos/#list-organization-repositories
